d3a079a71c4207e9ca245e7a20422f7f14ef5e6e
Illya Romaniv <mippikzilla@gmail.com>
2022-07-05 12:41:30 +0300

    remove console errors

--- src/app/components/builder/form-draggable-fields/form-draggable-fields.component.ts
+++ src/app/components/builder/form-draggable-fields/form-draggable-fields.component.ts
@@ -1,19 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {dragData} from "../../../../assets/data/dragData";
 import {Observable} from "rxjs";
 import {IActiveField} from "../../../../assets/models/IActiveField";
 import {select, Store} from "@ngrx/store";
 import {dragState, selectDefaultField} from "../../../../store/reducers/drag.reducer";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-form-draggable-fields',
   templateUrl: './form-draggable-fields.component.html',
   styleUrls: ['./form-draggable-fields.component.scss']
 })
-export class FormDraggableFieldsComponent {
+export class FormDraggableFieldsComponent implements OnInit{
   field$: Observable<IActiveField> | null;
   data = dragData;
-  constructor(private store: Store<dragState>) {
+  showForm: FormGroup;
+  constructor(private store: Store<dragState>, private fb: FormBuilder) {
     this.field$ = this.store.pipe(select(selectDefaultField))
+    this.showForm = new FormGroup({})
+  }
+  ngOnInit() {
+    this.field$?.subscribe(field => this.showForm.addControl(field.options.label, this.fb.control('')))
   }
 }
