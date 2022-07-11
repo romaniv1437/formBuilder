6baf6d535631e0948fa12c6940ba8069b3f4ab7d
Illya Romaniv <mippikzilla@gmail.com>
2022-07-11 14:30:06 +0300

    create e2e with cypress, fix types conflict(cypress, jasmine)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+
 import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
 import {AuthService} from "./service/auth.service";
