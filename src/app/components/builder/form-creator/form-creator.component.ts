1993d3922619a5f716bb073b89bce0cb16edfdfd
Illya Romaniv <mippikzilla@gmail.com>
2022-07-13 09:54:50 +0300

    now u can drag elements in form, and change the element position

--- src/app/components/builder/form-creator/form-creator.component.ts
+++ src/app/components/builder/form-creator/form-creator.component.ts
@@ -2,6 +2,7 @@ import {AfterContentChecked, ChangeDetectorRef, Component, Input} from '@angular
 import {Observable} from "rxjs";
 import {IActiveField} from "../../../../assets/models/IActiveField";
 import {FormGroup} from "@angular/forms";
+import {CdkDragDrop, moveItemInArray} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-form-creator',
@@ -14,14 +15,33 @@ export class FormCreatorComponent implements AfterContentChecked{
   @Input() form_result: FormGroup = new FormGroup<any>('')
   @Input() removeField: any;
   @Input() setEditMode: any;
+  formCopy: any;
+
+
 
   constructor(
     private ref: ChangeDetectorRef,
   ) {}
+  ngOnInit() {
+    this.form$?.subscribe(value => {
+      this.formCopy = value.map(field => field.field)
+    })
+  }
   ngAfterContentChecked() {
     this.ref.detectChanges();
   }
   onSubmit() {
     window.alert(JSON.stringify(this.form_result?.value))
   }
+
+  drop(event: CdkDragDrop<any>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
+
