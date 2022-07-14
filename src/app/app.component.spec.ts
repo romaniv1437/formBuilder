b87ab69f0f3d5e773831f2de2d64bb5a8f49dd6f
Illya Romaniv <mippikzilla@gmail.com>
2022-07-14 18:13:28 +0300

    form builder

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,75 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {AuthService} from "./service/auth.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {Location} from "@angular/common";
+import {Router} from "@angular/router";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 describe('AppComponent', () => {
+  let component: AppComponent
+  let fixture: ComponentFixture<AppComponent>;
+  let location: Location;
+  let router: Router
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,
+        MatSnackBarModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [AuthService]
     }).compileComponents();
+    location = TestBed.get(Location);
+    fixture = TestBed.createComponent(AppComponent);
+    router = TestBed.inject(Router);
+
+    component = fixture.componentInstance;
+    fixture.detectChanges()
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
+  it('should set token', () => {
 
-  it(`should have as title 'formBuilder'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('formBuilder');
+    // create spy on component method onInit
+    const spy = spyOn(component, "ngOnInit");
+
+    // in method onInit we set token from localstorage to authService
+    component.ngOnInit()
+
+    // expect ngOnInit to be called
+    expect(spy).toHaveBeenCalled()
   });
+  it('should redirect to "/" ' ,() => {
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('formBuilder app is running!');
+    // create spy to router
+    const spy = spyOn(router, 'navigateByUrl')
+
+    // call component function toHome()
+    component.toHome()
+
+    // get url path (will be '/')
+    const url = spy.calls.first().args[0]
+
+    // check if url equal '/'
+    expect(url).toBe('/')
+  });
+  it('should contain text "Angular Form Builder"', () => {
+
+    // get app content
+    const appContent: HTMLElement = fixture.nativeElement;
+
+    // select from app content h1
+    const headerText = appContent.querySelector('h1')!;
+
+    // check if h1 text to equal text that we want
+    expect(headerText.textContent).toEqual('Angular Form Builder');
   });
 });
